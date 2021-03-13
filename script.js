147c83778eea8382140b5d9eb4b07f07590be4d7
Natasha Mann <75756325+natasha-mann@users.noreply.github.com>
2021-03-13 14:09:07 +0000

    moved variables to global scope

--- script.js
+++ script.js
@@ -1,71 +1,72 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+const lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const upperCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const specialCharacterArray = ["!", "?", "£", "$", "%", "*", "&", "@", "#"];
+var password = "";
+
 // Declare a new function on line 3
 function generatePassword() {
   // Create a variable to store our generated password
-  var password = "";
-  //  Create variables for all the character types we can choose from
-  const lowerCaseArray = [
-    "a",
-    "b",
-    "c",
-    "d",
-    "e",
-    "f",
-    "g",
-    "h",
-    "i",
-    "j",
-    "k",
-    "l",
-    "m",
-    "n",
-    "o",
-    "p",
-    "q",
-    "r",
-    "s",
-    "t",
-    "u",
-    "v",
-    "w",
-    "x",
-    "y",
-    "z",
-  ];
-  const upperCaseArray = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z",
-  ];
-  const numbersArray = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  const specialCharacterArray = ["!", "?", "£", "$", "%", "*", "&", "@", "#"];
 
-  // Logic would go here
+  //  Create variables for all the character types we can choose from
+
   // Prompt for password length
   const passwordLength = prompt(
     "How many characters would you like your password to have?"
